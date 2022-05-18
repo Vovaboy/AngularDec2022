d6cefc1c53e6927a00c59363daa22fa6e407dca9
Vovaboy <vovaboy9510@gmail.com>
2022-05-18 18:50:20 +0200

    addd

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {UserService} from '../../services';
+import {IUser} from '../../interfaces';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
+  user: IUser;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private userService: UserService) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id}) => {
+      const state = this.router.getCurrentNavigation()?.extras?.state?.['user'] as IUser;
+
+      if (state) {
+        this.user = state
+      } else {
+        this.userService.getById(id).subscribe(value => this.user = value)
+      }
+    })
   }
 
 }
