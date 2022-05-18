8d841fbb7dc430764a303d02751e817624f49629
Vovaboy <vovaboy9510@gmail.com>
2022-05-18 19:36:02 +0200

    addd

--- src/app/components/post-details/post-details.component.ts
+++ src/app/components/post-details/post-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {PostService} from '../../services';
+import {IPost} from '../../interfaces';
 
 @Component({
   selector: 'app-post-details',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-details.component.css']
 })
 export class PostDetailsComponent implements OnInit {
+  post: IPost;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private postService: PostService) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id}) => {
+      const state = this.router.getCurrentNavigation()?.extras?.state?.['post'] as IPost;
+
+      if (state) {
+        this.post = state
+      } else {
+        this.postService.getById(id).subscribe(value => this.post = value)
+      }
+    })
+  }
 }
