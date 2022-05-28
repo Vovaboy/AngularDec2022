53417a38e2019a58710c653435582724ea8f24e0
Vovaboy <vovaboy9510@gmail.com>
2022-05-28 23:22:21 +0200

    17:59

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import {CarsComponent} from "./components/cars/cars.component";
+import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CarComponent,
+    CarsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+       HttpClientModule,
+      AppRoutingModule,
+      ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+form:FormGroup;
+
+constructor() {
+  this._createForm()
+}
+
+  ngOnInit(): void {
+  }
+_createForm(): void{
+  this.form= new FormGroup({
+    username:new FormControl(null, [Validators.minLength(2), Validators.maxLength(20)]),
+    password:new FormControl(null, [Validators.minLength(2), Validators.maxLength(20)]),
+    confirmPassword:new FormControl(null, [Validators.minLength(2), Validators.maxLength(20)])
+  },[this._checkPassword])
+}
+register():void{
+  console.log(this.form);
+}
+_checkPassword(form: AbstractControl): ValidationErrors | null{
+  const password = form.get('password')
+  const  confirmPassword= form.get('confirmPassword')
+  return password?.value === confirmPassword?.value ? null : {notSame: true}
+}
+}
